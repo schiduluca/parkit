d325488be7303413151d044f5c254851677a327e
Schidu Luca <schiduluca@gmail.com>
2017-06-06 19:05:53 +0300

    Initial commit. Added Thread library

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Thread.h>
+#include <ThreadController.h>
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+
+void loop() {
+
+  Serial.println("Hello world");
+  delay(100);
+}
